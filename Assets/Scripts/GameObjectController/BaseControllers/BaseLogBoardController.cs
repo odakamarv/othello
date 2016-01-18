58048ed1f20ae2797e6cc683a22d6508bec07224
kambe <kanbes@marv.jp>
2016-01-18 20:04:45 +0900

    BaseLogBoardController作成

---
+++ Assets/Scripts/GameObjectController/BaseControllers/BaseLogBoardController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BaseController
+{
+    public class BaseLogBoardController : MonoBehaviour
+    {
+        [SerializeField] private GameObject content;
+        [SerializeField] private Text logBoardOneLine;
+
+        public void AddLine(string text, Color color)
+        {
+            var addTo = content.transform;
+            var addLine = Instantiate(logBoardOneLine);
+
+            addLine.text = text;
+            addLine.color = color;
+            addLine.GetComponent<RectTransform>().SetParent(addTo, false);
+        }
+
+        public void RemoveLastLine()
+        {
+            var removeFrom = content.transform;
+            var removeLine = removeFrom.GetChild(removeFrom.childCount - 1);
+
+            removeLine.gameObject.GetComponent<RectTransform>().SetParent(null);
+            Destroy(removeLine.gameObject);
+        }
+    }
+}
