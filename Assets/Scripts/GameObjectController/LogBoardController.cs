6cbe3f6929143eaee0cb56ab18361ab9b67323c0
kambe <kanbes@marv.jp>
2016-01-18 11:58:05 +0900

    LogBoardControllerを作成

---
+++ Assets/Scripts/GameObjectController/LogBoardController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameObjectController
+{
+    public class LogBoardController : MonoBehaviour
+    {
+        [SerializeField] private GameObject content;
+        [SerializeField] private Text logBoardOneLine;
+
+        public void AddLine(string text, Color color)
+        {
+            var addTo = content.transform;
+            var addLine = GameObject.Instantiate(logBoardOneLine);
+
+            addLine.text = text;
+            addLine.color = color;
+            addLine.GetComponent<RectTransform>().SetParent(addTo, false);
+        }
+
+        public void RemoveLastLine()
+        {
+            var removeFrom = content.transform;
+            var removeLine = removeFrom.GetChild(removeFrom.childCount - 1);
+
+            removeLine.gameObject.GetComponent<RectTransform>().SetParent(null);
+            GameObject.Destroy(removeLine.gameObject);
+        }
+    }
+}
