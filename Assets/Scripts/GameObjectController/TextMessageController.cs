e9c8bcfa1180283ff0b8aea40eeed3cb8bdabc2d
kambe <kanbes@marv.jp>
2016-01-18 12:09:24 +0900

    TextMessageController作成

---
+++ Assets/Scripts/GameObjectController/TextMessageController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameObjectController
+{
+    public class TextMessageController : MonoBehaviour
+    {
+        [SerializeField] private Text textMessage;
+
+        public void SetTextMessage(string text, Color color)
+        {
+            textMessage.text = text;
+            textMessage.color = color;
+        }
+    }
+}
