bb46128de11aeab0fcf949ddc6db9db1a83bd768
kambe <kanbes@marv.jp>
2016-01-18 19:59:56 +0900

    BaseButtonController作成

---
+++ Assets/Scripts/GameObjectController/BaseController/BaseButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BaseController
+{
+    public class ButtonController : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+
+        public void SetEnabled()
+        {
+            button.interactable = true;
+        }
+
+        public void SetDisabled()
+        {
+            button.interactable = false;
+        }
+    }
+}
