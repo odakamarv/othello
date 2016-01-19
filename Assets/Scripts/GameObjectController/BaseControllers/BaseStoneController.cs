cc116a129f0ae889031415681f6daee425cb7856
kambe <kanbes@marv.jp>
2016-01-19 11:34:41 +0900

    BaseStoneController、StoneController作成

---
+++ Assets/Scripts/GameObjectController/BaseControllers/BaseStoneController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GameObjectController.BaseController
+{
+    public class BaseStoneController : MonoBehaviour
+    {
+        public void Turn()
+        {
+            GetComponentInChildren<Animator>().SetTrigger("doTurn");
+        }
+
+        public void setBlack()
+        {
+            GetComponentInChildren<Animator>().SetTrigger("setBlack");
+        }
+
+        public void setWhite()
+        {
+            GetComponentInChildren<Animator>().SetTrigger("setWhite");
+        }
+    }
+}
